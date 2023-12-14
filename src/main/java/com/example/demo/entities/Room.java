78b76592f38ec0965700791ea26a5830843f9938
Anton Tapper Dock <at@appsson.se>
2023-12-14 13:39:27 +0100

    added entities

--- src/main/java/com/example/demo/entities/Room.java
+++ src/main/java/com/example/demo/entities/Room.java
@@ -1,4 +1,49 @@
 package com.example.demo.entities;
 
+import jakarta.persistence.*;
+
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String roomNumber;
+    private String type;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
 }
