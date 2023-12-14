5cdd790cf01a44064af9249ca51e717bf1a316b8
Anton Tapper Dock <at@appsson.se>
2023-12-14 14:24:02 +0100

    added room

---
+++ src/main/java/com/example/demo/entities/Booking.java
@@ -0,0 +1,51 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(LocalDate checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public LocalDate getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(LocalDate checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+}
