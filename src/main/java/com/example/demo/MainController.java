197cd54f2abb3d6f7176d932a00108287052f05a
Anton Tapper Dock <at@appsson.se>
2023-12-14 14:48:37 +0100

    repository

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
